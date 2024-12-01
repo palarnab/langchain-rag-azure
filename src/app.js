f95b99b09f1c20cba3b770d6488d2ab143c7d0a0
Arnab Pal <arnab.i@gmail.com>
2024-12-01 14:55:12 +0530

    working sample with Dockerfile

--- src/app.js
+++ src/app.js
@@ -1,40 +1,38 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cookieParser = require('cookie-parser');
-const path = require('path');
-const fileupload = require('express-fileupload');
-const mongoSanitize = require('express-mongo-sanitize');
-const helmet = require('helmet');
-const xssClean = require('xss-clean');
-const rateLimit = require('express-rate-limit');
-const hpp = require('hpp');
-const compression = require('express-compression');
+const express = require("express");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const fileupload = require("express-fileupload");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xssClean = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const compression = require("express-compression");
 
-const { connectDB } = require('./config/db');
-const errorHandler = require('./middleware/error');
+const { connectDB } = require("./config/db");
+const errorHandler = require("./middleware/error");
 
 // load config
-dotenv.config({
-    path: path.join(__dirname, './config/config.env'),
-});
+dotenv.config();
 
 // connect to db
 connectDB();
 
 // Route files
-const admin = require('./services/admin/route');
-const auth = require('./services/auth/route');
-const chat = require('./services/chat/route');
+const admin = require("./services/admin/route");
+const auth = require("./services/auth/route");
+const chat = require("./services/chat/route");
 
 const app = express();
-app.set('trust proxy', true);
+app.set("trust proxy", true);
 
 // compression
 app.use(compression());
 
 // file upload
 app.use(fileupload());
-app.use(express.static(path.join(__dirname, './../public')));
+app.use(express.static(path.join(__dirname, "./../public")));
 
 // data parser middleware
 app.use(express.json());
@@ -47,14 +45,14 @@ app.use(mongoSanitize());
 
 // security headers
 app.use(
-    helmet({
-        contentSecurityPolicy: {
-            directives: {
-                ...helmet.contentSecurityPolicy.getDefaultDirectives(),
-                'script-src': ["'self'"],
-            },
-        },
-    })
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+        "script-src": ["'self'"],
+      },
+    },
+  })
 );
 
 // prevent xss attacks
@@ -62,10 +60,10 @@ app.use(xssClean());
 
 // rate limiting
 app.use(
-    rateLimit({
-        windowMs: 10 * 60 * 1000, // 10min
-        max: process.env.MAX_RATE_LIMIT || 200,
-    })
+  rateLimit({
+    windowMs: 10 * 60 * 1000, // 10min
+    max: process.env.MAX_RATE_LIMIT || 200,
+  })
 );
 
 // prevent http param pollution
@@ -73,17 +71,17 @@ app.use(hpp());
 
 // server identification
 app.use(function (req, res, next) {
-    res.setHeader('lyx-node', process.env.SERVERNAME);
-    next();
+  res.setHeader("rag-azure", process.env.SERVERNAME);
+  next();
 });
 
-app.get('/health', (req, res) => {
-    res.status(200).send('healthy');
+app.get("/health", (req, res) => {
+  res.status(200).send("healthy");
 });
 
-app.use('/api/v1/admin', admin);
-app.use('/api/v1/auth', auth);
-app.use('/api/v1/chat', chat);
+app.use("/api/v1/admin", admin);
+app.use("/api/v1/auth", auth);
+app.use("/api/v1/chat", chat);
 
 app.use(errorHandler);
 
